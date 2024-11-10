594bc5efd85078c4fcb5c7669da14c94e6faf36a
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-11-10 10:51:46 -0500

    Registro de nuevo ms

--- apigateway.tf
+++ apigateway.tf
@@ -43,6 +43,7 @@ locals {
     incidentmodify_url = "https://incidentmodify-${data.google_project.default.number}.${local.region}.run.app"
     incidentquery_url = "https://incidentquery-${data.google_project.default.number}.${local.region}.run.app"
     registroapp_url = "https://registroapp-${data.google_project.default.number}.${local.region}.run.app"
+    invoice_url = "https://invoice-${data.google_project.default.number}.${local.region}.run.app"
   })
 }
 
